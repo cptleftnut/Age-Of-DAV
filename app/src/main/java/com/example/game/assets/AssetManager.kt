301029ccd2f5af928bdb9764fc2eb6df1e69df24
cptleftnut <78494323+cptleftnut@users.noreply.github.com>
2026-08-06 20:35:59 +0200

    Update AssetManager to use PNG sprites instead of XML drawables

--- app/src/main/java/com/example/game/assets/AssetManager.kt
+++ app/src/main/java/com/example/game/assets/AssetManager.kt
@@ -5,103 +5,103 @@ import com.example.R
 import com.example.game.model.*
 
 /**
- * Central asset registry mapping all game entities to their vector drawable resources.
- * Replaces emoji placeholders with proper scalable vector icons.
+ * Central asset registry mapping all game entities to their PNG sprite resources.
+ * Replaces emoji placeholders with proper game sprites.
  */
 object AssetManager {
 
     @DrawableRes
     fun getUnitDrawable(type: UnitType): Int = when (type) {
-        UnitType.VILLAGER -> R.drawable.ic_unit_villager
-        UnitType.DWARVEN_MINER -> R.drawable.ic_unit_dwarven_miner
-        UnitType.HOPLITE -> R.drawable.ic_unit_hoplite
-        UnitType.SPEARMAN -> R.drawable.ic_unit_spearman
-        UnitType.TOXOTES_ARCHER -> R.drawable.ic_unit_toxotes
-        UnitType.PRODROMOS_CAVALRY -> R.drawable.ic_unit_cavalry
-        UnitType.THROWING_AXEMAN -> R.drawable.ic_unit_axeman
-        UnitType.HERCULES -> R.drawable.ic_unit_hercules
-        UnitType.PHARAOH -> R.drawable.ic_unit_pharaoh
-        UnitType.HERSIR -> R.drawable.ic_unit_hersir
-        UnitType.MINOTAUR -> R.drawable.ic_unit_minotaur
-        UnitType.CYCLOPS -> R.drawable.ic_unit_cyclops
-        UnitType.VALKYRIE -> R.drawable.ic_unit_valkyrie
-        UnitType.SPHINX -> R.drawable.ic_unit_sphinx
-        UnitType.TITAN -> R.drawable.ic_unit_titan
+        UnitType.VILLAGER -> R.drawable.sprite_unit_villager
+        UnitType.DWARVEN_MINER -> R.drawable.sprite_unit_dwarven_miner
+        UnitType.HOPLITE -> R.drawable.sprite_unit_hoplite
+        UnitType.SPEARMAN -> R.drawable.sprite_unit_spearman
+        UnitType.TOXOTES_ARCHER -> R.drawable.sprite_unit_toxotes
+        UnitType.PRODROMOS_CAVALRY -> R.drawable.sprite_unit_cavalry
+        UnitType.THROWING_AXEMAN -> R.drawable.sprite_unit_axeman
+        UnitType.HERCULES -> R.drawable.sprite_unit_hercules
+        UnitType.PHARAOH -> R.drawable.sprite_unit_pharaoh
+        UnitType.HERSIR -> R.drawable.sprite_unit_hersir
+        UnitType.MINOTAUR -> R.drawable.sprite_unit_minotaur
+        UnitType.CYCLOPS -> R.drawable.sprite_unit_cyclops
+        UnitType.VALKYRIE -> R.drawable.sprite_unit_valkyrie
+        UnitType.SPHINX -> R.drawable.sprite_unit_sphinx
+        UnitType.TITAN -> R.drawable.sprite_unit_titan
     }
 
     @DrawableRes
     fun getBuildingDrawable(type: BuildingType): Int = when (type) {
-        BuildingType.TOWN_CENTER -> R.drawable.ic_building_town_center
-        BuildingType.HOUSE -> R.drawable.ic_building_house
-        BuildingType.FARM -> R.drawable.ic_building_farm
-        BuildingType.GRANARY -> R.drawable.ic_building_granary
-        BuildingType.STOREHOUSE -> R.drawable.ic_building_storehouse
-        BuildingType.BARRACKS -> R.drawable.ic_building_barracks
-        BuildingType.TEMPLE -> R.drawable.ic_building_temple
-        BuildingType.ARMORY -> R.drawable.ic_building_armory
-        BuildingType.WONDER -> R.drawable.ic_building_wonder
+        BuildingType.TOWN_CENTER -> R.drawable.sprite_building_town_center
+        BuildingType.HOUSE -> R.drawable.sprite_building_house
+        BuildingType.FARM -> R.drawable.sprite_building_farm
+        BuildingType.GRANARY -> R.drawable.sprite_building_granary
+        BuildingType.STOREHOUSE -> R.drawable.sprite_building_storehouse
+        BuildingType.BARRACKS -> R.drawable.sprite_building_barracks
+        BuildingType.TEMPLE -> R.drawable.sprite_building_temple
+        BuildingType.ARMORY -> R.drawable.sprite_building_armory
+        BuildingType.WONDER -> R.drawable.sprite_building_wonder
     }
 
     @DrawableRes
     fun getResourceDrawable(type: ResourceNodeType): Int = when (type) {
-        ResourceNodeType.GOLD_MINE -> R.drawable.ic_resource_gold
-        ResourceNodeType.FOREST_TREE -> R.drawable.ic_resource_wood
-        ResourceNodeType.BERRY_BUSH -> R.drawable.ic_resource_food
-        ResourceNodeType.HUNT_ANIMAL -> R.drawable.ic_resource_food
-        ResourceNodeType.RELIC -> R.drawable.ic_resource_favor
+        ResourceNodeType.GOLD_MINE -> R.drawable.sprite_resource_gold
+        ResourceNodeType.FOREST_TREE -> R.drawable.sprite_resource_wood
+        ResourceNodeType.BERRY_BUSH -> R.drawable.sprite_resource_food
+        ResourceNodeType.HUNT_ANIMAL -> R.drawable.sprite_resource_food
+        ResourceNodeType.RELIC -> R.drawable.sprite_resource_favor
     }
 
     @DrawableRes
     fun getFactionDrawable(faction: Faction): Int = when (faction) {
-        Faction.GREEK -> R.drawable.ic_faction_greek
-        Faction.EGYPTIAN -> R.drawable.ic_faction_egyptian
-        Faction.NORSE -> R.drawable.ic_faction_norse
-        Faction.ATLANTEAN -> R.drawable.ic_faction_atlantean
+        Faction.GREEK -> R.drawable.sprite_faction_greek
+        Faction.EGYPTIAN -> R.drawable.sprite_faction_egyptian
+        Faction.NORSE -> R.drawable.sprite_faction_norse
+        Faction.ATLANTEAN -> R.drawable.sprite_faction_atlantean
     }
 
     @DrawableRes
     fun getGodPowerDrawable(power: GodPower): Int = when (power) {
-        GodPower.LIGHTNING_BOLT -> R.drawable.ic_gp_lightning
-        GodPower.METEOR -> R.drawable.ic_gp_meteor
-        GodPower.HEALING_SPRING -> R.drawable.ic_gp_healing
-        GodPower.RESTORATION -> R.drawable.ic_gp_restoration
-        GodPower.CEASEFIRE -> R.drawable.ic_gp_ceasefire
-        GodPower.GREAT_HUNT -> R.drawable.ic_gp_great_hunt
-        GodPower.FROST -> R.drawable.ic_gp_frost
-        GodPower.SHOCKWAVE -> R.drawable.ic_gp_shockwave
-        GodPower.TORNADO -> R.drawable.ic_gp_tornado
-        GodPower.RAIN -> R.drawable.ic_gp_rain
-        GodPower.PROSPERITY -> R.drawable.ic_gp_prosperity
-        GodPower.UNDERWORLD_PASSAGE -> R.drawable.ic_gp_passage
-        GodPower.BRONZE -> R.drawable.ic_gp_bronze
-        GodPower.PLENTY_VAULT -> R.drawable.ic_gp_plenty
-        GodPower.RAGNAROK -> R.drawable.ic_gp_ragnarok
-        GodPower.VISION -> R.drawable.ic_gp_vision
-        GodPower.DWARVEN_MINE -> R.drawable.ic_gp_dwarven_mine
-        GodPower.SPY -> R.drawable.ic_gp_spy
-        GodPower.GAIA_FOREST -> R.drawable.ic_gp_gaia_forest
-        GodPower.PESTILENCE -> R.drawable.ic_gp_pestilence
-        GodPower.PLAGUE_OF_SERPENTS -> R.drawable.ic_gp_serpents
-        GodPower.ECLIPSE -> R.drawable.ic_gp_eclipse
-        GodPower.CITADEL -> R.drawable.ic_gp_citadel
-        GodPower.WINTER_BLAST -> R.drawable.ic_gp_winter
+        GodPower.LIGHTNING_BOLT -> R.drawable.sprite_gp_lightning
+        GodPower.METEOR -> R.drawable.sprite_gp_meteor
+        GodPower.HEALING_SPRING -> R.drawable.sprite_gp_healing
+        GodPower.RESTORATION -> R.drawable.sprite_gp_restoration
+        GodPower.CEASEFIRE -> R.drawable.sprite_gp_ceasefire
+        GodPower.GREAT_HUNT -> R.drawable.sprite_gp_great_hunt
+        GodPower.FROST -> R.drawable.sprite_gp_frost
+        GodPower.SHOCKWAVE -> R.drawable.sprite_gp_shockwave
+        GodPower.TORNADO -> R.drawable.sprite_gp_tornado
+        GodPower.RAIN -> R.drawable.sprite_gp_rain
+        GodPower.PROSPERITY -> R.drawable.sprite_gp_prosperity
+        GodPower.UNDERWORLD_PASSAGE -> R.drawable.sprite_gp_passage
+        GodPower.BRONZE -> R.drawable.sprite_gp_bronze
+        GodPower.PLENTY_VAULT -> R.drawable.sprite_gp_plenty
+        GodPower.RAGNAROK -> R.drawable.sprite_gp_ragnarok
+        GodPower.VISION -> R.drawable.sprite_gp_vision
+        GodPower.DWARVEN_MINE -> R.drawable.sprite_gp_dwarven_mine
+        GodPower.SPY -> R.drawable.sprite_gp_spy
+        GodPower.GAIA_FOREST -> R.drawable.sprite_gp_gaia_forest
+        GodPower.PESTILENCE -> R.drawable.sprite_gp_pestilence
+        GodPower.PLAGUE_OF_SERPENTS -> R.drawable.sprite_gp_serpents
+        GodPower.ECLIPSE -> R.drawable.sprite_gp_eclipse
+        GodPower.CITADEL -> R.drawable.sprite_gp_citadel
+        GodPower.WINTER_BLAST -> R.drawable.sprite_gp_winter
     }
 
     @DrawableRes
     fun getTerrainDrawable(type: TerrainType): Int = when (type) {
-        TerrainType.GRASS -> R.drawable.ic_terrain_grass
-        TerrainType.DESERT_SAND -> R.drawable.ic_terrain_desert
-        TerrainType.SNOW -> R.drawable.ic_terrain_snow
-        TerrainType.WATER -> R.drawable.ic_terrain_water
-        TerrainType.ROCK -> R.drawable.ic_terrain_rock
+        TerrainType.GRASS -> R.drawable.sprite_terrain_grass
+        TerrainType.DESERT_SAND -> R.drawable.sprite_terrain_desert
+        TerrainType.SNOW -> R.drawable.sprite_terrain_snow
+        TerrainType.WATER -> R.drawable.sprite_terrain_water
+        TerrainType.ROCK -> R.drawable.sprite_terrain_rock
     }
 
     @DrawableRes
     fun getGatherResourceDrawable(type: GatherResourceType): Int = when (type) {
-        GatherResourceType.FOOD -> R.drawable.ic_resource_food
-        GatherResourceType.WOOD -> R.drawable.ic_resource_wood
-        GatherResourceType.GOLD -> R.drawable.ic_resource_gold
-        GatherResourceType.FAVOR -> R.drawable.ic_resource_favor
+        GatherResourceType.FOOD -> R.drawable.sprite_resource_food
+        GatherResourceType.WOOD -> R.drawable.sprite_resource_wood
+        GatherResourceType.GOLD -> R.drawable.sprite_resource_gold
+        GatherResourceType.FAVOR -> R.drawable.sprite_resource_favor
     }
 }
 
